6c47c92a3ec7a7b143550f0d63ec5b5f9471c6e9
shaarang <shaarang.1@hotmail.com>
2020-01-18 22:19:50 +1100

    üpdated scrapper code to allow for undefined

--- helpers/scraper.js
+++ helpers/scraper.js
@@ -8,25 +8,31 @@ const scrapePage = (arg, elem) =>{
     const elements = $(elem);
 
     let bikes=[];
-
-    //console.log(typeof(elements));
     for (let i = 0; i < elements.length; i++){
         const findEle = $(elements[i]).find('a.product-tile__link');
-        if(findEle){
-            /*for(let j = 0; j < findEle.length; j++){
-                // take attrib from the element
-
-                console.log(findEle[j].attribs);
-                const imgFile = $(findEle[j]).find('img.product-tile__image');
-                console.log(imgFile);
-            }*/
-            console.log(findEle[1].attribs);
+        let element = {}
+        if(findEle[1] !== undefined){
+            element = findEle[1];
+        }
+        else{
+            element = findEle[0];
+        }
+        if(element){
+            let data = {
+                'bikeTitle': element.attribs['title'],
+                'bikeBrand': element.attribs['data-brand'],
+                'category': element.attribs['data-category-name'],
+                'price': parseFloat(element.attribs['data-price'])
+            }
+            console.log(element.attribs);
             const imgFile = $(findEle).find('img.product-tile__image.lazyload');
+            data['image'] = imgFile[0].attribs['data-src']; 
             console.log(imgFile[0].attribs['data-src']);
-
-            break;
+            bikes.push(data);
         }
     }
+    console.log("BIKES -------------- ");
+    console.log(bikes);
 }
 
 module.exports.scrapePage = scrapePage;
